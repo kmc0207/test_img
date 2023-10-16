9536288dcd886b18697b15b0e8c0313f15619270
kmc0207 <53795870+kmc0207@users.noreply.github.com>
2023-10-16 10:32:57 +0900

    Update test.py

--- test.py
+++ test.py
@@ -29,19 +29,42 @@ photo_list = {
     'non-hr': os.listdir('../data/haerin_bench/non-haerin')
 }
 
-
 prompts = [
 '''The following is a conversation between a curious human and AI assistant. The assistant gives helpful, detailed, and polite answers to the user's questions.
 Human: <image>
-Human: Who is she?
+Human: What is her name?
 AI: ''']
 
 images = [Image.open('../data/haerin_bench/haerin/' + photo_list['hr'][0])]
 
+
+def test(): 
+    inputs = processor(text = prompts, images =images,return_tensors='pt')
+    inputs = {k:v.bfloat16() if v.dtype == torch.float else v for k, v in inputs.items()}
+    inputs = {k : v.to(model.device) for k,v in inputs.items()}
+    with torch.no_grad():
+        res = model.generate(**inputs,**generate_kwargs)
+    sentence = tokenizere.decode(res.tolist()[0],skip_special_tokens=True)
+    return sentence
+
+print(test())
+
+
+label_textt = 'Her Name is Haerin'
+label = tokenizer(label_text,return_tensor='pt').to(device)
 inputs = processor(text = prompts, images =images,return_tensors='pt')
 inputs = {k:v.bfloat16() if v.dtype == torch.float else v for k, v in inputs.items()}
 inputs = {k : v.to(model.device) for k,v in inputs.items()}
-with torch.no_grad():
-    res = model.generate(**inputs,**generate_kwargs)
-sentence = tokenizere.decode(res.tolist()[0],skip_special_tokens=True)
-print(sentence)
+print('input_ids :', inputs['input_ids'], inputs['input_ids'].size())
+print('label :', label, label.size())
+
+#Training only LM
+
+
+
+#Training only Vision Model (Maybe Impossible)
+
+
+
+
+#Training for Similarity Training
